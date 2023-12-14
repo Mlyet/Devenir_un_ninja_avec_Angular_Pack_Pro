ca875cabdafdf4c24fb187d634fcfe35003c9e58
Thomas <thomas.meyet@epitech.eu>
2023-12-14 15:07:50 +0100

    Exercice 19

--- ponyracer_started/ponyracer/src/app/user.service.ts
+++ ponyracer_started/ponyracer/src/app/user.service.ts
@@ -12,7 +12,9 @@ export class UserService {
   userUrl = 'https://ponyracer.ninja-squad.com';
   userEvents = new BehaviorSubject<UserModel | null>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.retrieveUser();
+  }
 
   register(login: string, password: string, birthYear: number): Observable<UserModel> {
     const body = { login, password, birthYear };
@@ -22,6 +24,19 @@ export class UserService {
   authenticate(credentials: { login: string; password: string }): Observable<UserModel> {
     return this.http
       .post<UserModel>(this.userUrl + '/api/users/authentication', credentials)
-      .pipe(tap((user: UserModel) => this.userEvents.next(user)));
+      .pipe(tap(user => this.storeLoggedInUser(user)));
+  }
+
+  storeLoggedInUser(user: UserModel): void {
+    window.localStorage.setItem('rememberMe', JSON.stringify(user));
+    this.userEvents.next(user);
+  }
+
+  retrieveUser(): void {
+    const value = window.localStorage.getItem('rememberMe');
+    if (value) {
+      const user = JSON.parse(value) as UserModel;
+      this.userEvents.next(user);
+    }
   }
 }
