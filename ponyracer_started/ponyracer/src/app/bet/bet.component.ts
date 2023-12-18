e6776bb545b60b072922fa835184f6b1c7c8e3dc
Thomas <thomas.meyet@epitech.eu>
2023-12-18 17:49:46 +0100

    Exercice 23

---
+++ ponyracer_started/ponyracer/src/app/bet/bet.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RaceModel } from '../models/race.model';
+import { ActivatedRoute } from '@angular/router';
+import { RaceService } from '../race.service';
+import { PonyModel } from '../models/pony.model';
+
+@Component({
+  selector: 'pr-bet',
+  templateUrl: './bet.component.html',
+  styleUrls: ['./bet.component.css']
+})
+export class BetComponent {
+  raceModel: RaceModel | null = null;
+  betFailed = false;
+
+  constructor(private raceService: RaceService, private route: ActivatedRoute) {
+    const raceId = +this.route.snapshot.paramMap.get('raceId')!;
+    this.raceService.get(raceId).subscribe(race => (this.raceModel = race));
+  }
+
+  betOnPony(pony: PonyModel): void {
+    this.raceService.bet(this.raceModel!.id, pony.id).subscribe({
+      next: race => (this.raceModel = race),
+      error: () => (this.betFailed = true)
+    });
+  }
+
+  isPonySelected(pony: PonyModel): boolean {
+    return pony.id === this.raceModel!.betPonyId;
+  }
+}
--- ponyracer_started/ponyracer/src/app/race.service.ts
+++ ponyracer_started/ponyracer/src/app/race.service.ts
@@ -15,4 +15,11 @@ export class RaceService {
   list(): Observable<Array<RaceModel>> {
     return this.http.get<Array<RaceModel>>(environment.baseUrl + '/api/races?status=PENDING');
   }
+  bet(raceId: number, ponyId: number) {
+    return this.http.post<RaceModel>(environment.baseUrl + `/api/races/${raceId}/bets`, { ponyId });
+  }
+
+  get(raceId: number): Observable<RaceModel> {
+    return this.http.get<RaceModel>(environment.baseUrl + '/api/races/' + raceId);
+  }
 }
